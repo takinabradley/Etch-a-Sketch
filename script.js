613fad8c00b3039808a7af5a5777c11e717c578a
takinabradley <bradfinney98@gmail.com>
2021-10-22 20:00:04 -0400

    commented out some old code and explained why it doesn't work for my benefit later.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createGrid (size) {
     row.forEach ( row => row.appendChild(items.cloneNode(true)) );
   }
   
-  
+  // Below code doesn't work because 'items' isn't cloned to be used again.
   // row[1] = document.createElement('div');
   // row[1].classList.add('_1');
   // 
@@ -31,36 +31,5 @@ function createGrid (size) {
   // grid.appendChild(row[1]);
   // grid.appendChild(row[2]);
 }
-  //maybe append rows first?
-  /* why are all the items only appended to the last fuggin row slakjfkjfa */
   
 createGrid(16);
-
-/*https://www.geeksforgeeks.org/how-to-use-dynamic-variable-names-in-javascript/
-  maybe this will help?*/
-
-
-/* 
-  function createGrid () {
-  grid = document.querySelector('#grid');
-  
-  row1 = document.createElement('div');
-  row1.classList.add('_1');
-  
-  row2 = document.createElement('div');
-  row2.classList.add('_2');
-  
-  items1 = document.createElement('div');
-  items2 = document.createElement('div');
-  
-  row1.appendChild(items1);
-  row1.appendChild(items2);
-  
-  row2.appendChild(items1);
-  row2.appendChild(items2);
-  
-  grid.appendChild(row1);
-  grid.appendChild(row2);
-    
-}
-*/
