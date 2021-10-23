ff24a7ab9c7cb70e7c2852bb0cc62d7fcfc7e893
takinabradley <bradfinney98@gmail.com>
2021-10-23 02:22:28 -0400

    moved functinality to clear the grid to a clearGrid() function. Two new functions were created in anticipation of a 'wipe sketchpad' button in the UI- namely findGridSize() and wipeSketchPad().

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function createGrid (size) {
   grid = document.querySelector('#grid');
   
-  grid.innerHTML = '';
-  //clears any previous grid contents every time the function is run.
+  clearGrid();
   
   for (i = 0; i < size; i++) {
     row = document.createElement('div');
@@ -19,25 +18,52 @@ function createGrid (size) {
     
     rows.forEach ( row => row.appendChild(cell.cloneNode(true)) );
     /*cell.cloneNod(true) allows the cells to be cloned to each row, instead
-      of assigned to a row, then reassigned to another row on each row.forEach loop*/
+      of assigned to a row, then reassigned to another row on each 'row.forEach'
+      loop*/
   }
   //adds 'size' amount of cells to each row.
-  
 }
 //creates a 'size'x'size' grid.
+
+
+function clearGrid() {
+  document.querySelector('#grid').innerHTML = '';
+}
+//clears any previous grid contents.
+
+
+function findGridSize () {
+  return document.querySelectorAll('#grid > div').length;
+}
+//counts the rows in the #grid container and returns the grid size.
+
 
 function allowDraw () {
   cells = document.querySelectorAll('#grid > div > div');
   
-  cells.forEach( cell => cell.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = "black"));
+  cells.forEach( cell => cell.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = "black";
+  }));
 }
-//draw needs to be applied every time a new grid is created.
+/*allowDraw applies event listeners to each cell of the grid.
+  Needs to be applied every time a new grid is created.*/
+
 
 function createSketchPad (size) {
   createGrid(size);
   allowDraw();
 }
 //creates a grid with with drawing allowed
+
+
+function wipeSketchPad() {
+  size = findGridSize();
+  
+  createSketchPad(size);
+}
+/*clears the current sketchpad, then replaces it with a sketchpad of the same 
+  size*/
+  
 
 createSketchPad(16);
 
